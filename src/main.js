81f05e5f8fdbc70ccca98d544932b9d85d4327fc
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-03-02 00:21:43 -0500

    feat: Initial structure

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const contenedor = document.querySelector('.gallery');
+
+let contenido = '';
+
+let urls = [
+  'https://images.unsplash.com/photo-1646112918482-2763d6e12320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1646152159606-2b5aca677290?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1646145752399-98b6c2f2af0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1646143612209-a06a538b8c7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1646006304108-50b55c79631a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1638913974023-cef988e81629?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1646130705727-7682a50aadb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+];
+
+for (let i = 0; i < 100; i++) {
+  contenido += `<imagen-element url="${urls[i % urls.length]}"/>`;
+}
+contenedor.innerHTML = contenido;
