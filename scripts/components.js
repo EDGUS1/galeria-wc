2fd3c32e2eb5e0ee9358d5811c0f643bb4478d79
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2022-03-04 11:42:31 -0500

    feat: Add new element

--- scripts/components.js
+++ scripts/components.js
@@ -3,12 +3,16 @@ class ImagenElement extends HTMLElement {
     super();
     this.url =
       'https://images.unsplash.com/photo-1646112918482-2763d6e12320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60';
+    this.text = '';
   }
 
   connectedCallback() {
     let imagen = document.createElement('img');
+    let texto = document.createElement('span');
     imagen.src = this.url;
+    texto.textContent = this.text;
     this.appendChild(imagen);
+    this.appendChild(texto);
   }
 
   attributeChangedCallback(nombre, oldValue, newValue) {
